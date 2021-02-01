938f8de5baca86b76250a8e55af23b2c250d48e8
Matthew <mattk.development@gmail.com>
2021-01-31 19:17:52 -0600

    Product Rebuilt

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+
+import React, { useState } from 'react'
+import { useDarkMode } from '../components/hooks/useDarkMode'
+
+const Navbar = ({ darkMode, setDarkMode }) => {
+  const toggleMode = (e) => {
+    e.preventDefault()
+    setDarkMode(!darkMode)
+  }
+  return (
+    <nav className='navbar'>
+      <h1>Crypto Tracker</h1>
+      <div className='dark-mode__toggle'>
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
